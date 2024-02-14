2e4b8466bc02d0162b749609a7757c9cba6b8dec
grace-apa <149172807+grace-apa@users.noreply.github.com>
2024-02-14 13:34:40 +0000

    promptMenu added to index.js file

--- starter/index.js
+++ starter/index.js
@@ -50,3 +50,69 @@ function promptManager() {
       promptMenu();
     });
 }
+
+function promptIntern() {
+  console.log("Please enter the intern's information:");
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Name:",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "ID:",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Email:",
+      },
+      {
+        type: "input",
+        name: "school",
+        message: "School:",
+      },
+    ])
+    .then((answers) => {
+      const intern = new Intern(
+        answers.name,
+        answers.id,
+        answers.email,
+        answers.school
+      );
+      team.push(intern);
+      promptMenu();
+    });
+}
+
+function promptMenu() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "choice",
+        message: "Would you like to:",
+        choices: [
+          "Add an engineer",
+          "Add an intern",
+          "Finish building the team",
+        ],
+      },
+    ])
+    .then((answer) => {
+      switch (answer.choice) {
+        case "Add an engineer":
+          promptEngineer();
+          break;
+        case "Add an intern":
+          promptIntern();
+          break;
+        case "Finish building the team":
+          renderTeam();
+          break;
+      }
+    });
+}
