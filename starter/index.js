b0a482cf7aab3453de64167afc937ba672cb4193
grace-apa <149172807+grace-apa@users.noreply.github.com>
2024-02-14 13:12:44 +0000

    user prompted to answer questions about team manager

--- starter/index.js
+++ starter/index.js
@@ -13,3 +13,40 @@ const render = require("./src/page-template.js");
 const team = [];
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
+
+function promptManager() {
+  console.log("Please enter the team manager's information:");
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Name:",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "ID:",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Email:",
+      },
+      {
+        type: "input",
+        name: "officeNumber",
+        message: "Office Number:",
+      },
+    ])
+    .then((answers) => {
+      const manager = new Manager(
+        answers.name,
+        answers.id,
+        answers.email,
+        answers.officeNumber
+      );
+      team.push(manager);
+      promptMenu();
+    });
+}
